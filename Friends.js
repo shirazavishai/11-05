d391bd70de53781c22c5f31fd072f5fe7afc6bd3
eyalTam <48117460+eyalTam@users.noreply.github.com>
2020-05-10 21:54:55 +0300

    Add files via upload

---
+++ Friends.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class Friends extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#friends');
+ReactDOM.render(e(LikeButton), domContainer);
